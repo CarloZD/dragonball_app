889b4d2b5eb0fd3ed6da9c3af2bbb5d75e132891
CarloZD <carlos.ruiz.l@tecsup.edu.pe>
2025-11-01 22:52:02 -0500

    1ra parte de home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+const App = () => {
   return (
-    <div>
-      <h1>Dragon Ball App</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroSection from "../components/home/HeroSection";
+import PopularSection from "../components/home/PopularSection";
+
+const HomePage = () => {
+  return (
+    <div>
+      <HeroSection />
+      <PopularSection />
+    </div>
+  );
+};
+
+export default HomePage;
