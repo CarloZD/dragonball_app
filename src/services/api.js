154fba103af64cd5a37bf0ee0dd9835ba12333d5
jru-dev <holasoyhotaru@gmail.com>
2025-11-01 23:20:22 -0500

    Update api.js

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://dragonball-api.com/api",
+  timeout: 10000, 
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("Error en la petición:", error);
+    return Promise.reject(error);
+  }
+);
+
+export default api;
